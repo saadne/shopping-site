6162ce6e89c29e1dc245ffd70fb67395e3935655
saadne <saadne2018@gmail.com>
2020-08-22 21:04:51 +0000

    broken our homepage into small components and styling our menu

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,42 +1,12 @@
 import React from "react";
 
 import "./Homepage.styles.scss";
+import Directory from "../../components/directory/Directory";
 
 function Homepage() {
   return (
     <div className="homepage">
-      <div className="items">
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-      </div>
+      <Directory />
     </div>
   );
 }
